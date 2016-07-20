bc2bf8bbf76ac9dae0434c2db52c28ffa01dc00f
Dimitry Besson <dimitrybesson@gmail.com>
2016-07-20 14:33:33 -0400

    Add tests for order attributes

--- packaging-test.rb
+++ packaging-test.rb
@@ -3,8 +3,23 @@ require 'packaging'
 
 class TestOrderPricing < MiniTest::Test
 
-  def test_order_object_is_instance_of_order
+  def setup
     @order = Order.new
+  end
+
+  def test_order_object_is_instance_of_order
     assert_instance_of(Order, @order)
+  end
+
+  def test_order_has_base_price_attribute
+    assert_equal(@order.base_price, nil)
+  end
+
+  def test_order_has_number_of_workers_attribute
+    assert_equal(@order.number_of_workers, nil)
+  end
+
+  def test_order_has_product_type_attribute
+    assert_equal(@order.product_type, nil)
   end
 end
--- packaging.rb
+++ packaging.rb
@@ -1,2 +1,8 @@
 class Order
+  attr_reader :base_price, :number_of_workers, :product_type
+  def initialize
+    @base_price = nil
+    @number_of_workers = nil
+    @product_type = nil
+  end
 end
