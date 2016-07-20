a5e2a5fd658181ce1c109a0a4ef0925e67ce2596
Dimitry Besson <dimitrybesson@gmail.com>
2016-07-20 16:00:17 -0400

    Add markup rates hash

--- packaging-test.rb
+++ packaging-test.rb
@@ -4,37 +4,42 @@ require 'packaging'
 class TestOrderPricing < MiniTest::Test
 
   def setup
-    @order = Order.new()
-    @order1 = Order.new(base_price: 1299.99, number_of_workers: 3, product_type: 'food')
+    @order1 = Order.new()
+    @order2 = Order.new(base_price: 1299.99, number_of_workers: 3, product_type: 'food')
   end
 
   def test_order_object_is_instance_of_order
-    assert_instance_of(Order, @order)
+    assert_instance_of(Order, @order1)
   end
 
   def test_order_has_base_price_attribute
-    assert_equal(@order.base_price, nil)
+    assert_equal(@order1.base_price, nil)
   end
 
   def test_order_has_number_of_workers_attribute
-    assert_equal(@order.number_of_workers, nil)
+    assert_equal(@order1.number_of_workers, nil)
   end
 
   def test_order_has_product_type_attribute
-    assert_equal(@order.product_type, nil)
+    assert_equal(@order1.product_type, nil)
   end
 
   def test_order_has_total_price_attribute
-    assert_equal(@order.total_price, nil)
+    assert_equal(@order1.total_price, nil)
   end
 
   def test_order_sets_input_attributes
-    order_attributes = [@order1.base_price, @order1.number_of_workers, @order1.product_type]
+    order_attributes = [@order2.base_price, @order2.number_of_workers, @order2.product_type]
     assert_equal([1299.99, 3, 'food'], order_attributes)
   end
 
   def test_get_worker_markup_rate
-    worker_markup = WORKER_MARKUP * @order1.number_of_workers
-    assert_equal(worker_markup, @order1.get_worker_markup_rate)
+    worker_markup = MARKUP_RATES[:worker_markup] * @order2.number_of_workers
+    assert_equal(worker_markup, @order2.get_worker_markup_rate)
+  end
+
+  def test_get_product_type_markup_rate
+    product_type_markup_rate = MARKUP_RATES[:product_type_markup][@order2.product_type.to_sym]
+    assert_equal(product_type_markup_rate, @order2.get_product_type_markup_rate)
   end
 end
--- packaging.rb
+++ packaging.rb
@@ -1,4 +1,7 @@
-WORKER_MARKUP = 0.012
+MARKUP_RATES = { flat_markup: 0.05,
+                 worker_markup: 0.012,
+                 product_type_markup: { 'drugs': 0.075, 'food': 0.13, 'electronics': 0.02 }
+              }
 
 class Order
   attr_reader :base_price, :number_of_workers, :product_type, :total_price
@@ -10,6 +13,10 @@ class Order
   end
 
   def get_worker_markup_rate
-    @number_of_workers * WORKER_MARKUP
+    @number_of_workers * MARKUP_RATES[:worker_markup]
+  end
+
+  def get_product_type_markup_rate
+    MARKUP_RATES[:product_type_markup][@product_type.to_sym]
   end
 end
