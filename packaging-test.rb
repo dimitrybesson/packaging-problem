2c1192410d24873f35364762786b99506d62298e
Dimitry Besson <dimitrybesson@gmail.com>
2016-07-20 14:25:34 -0400

    Add first test for order existance

--- packaging-test.rb
+++ packaging-test.rb
@@ -3,4 +3,8 @@ require 'packaging'
 
 class TestOrderPricing < MiniTest::Test
 
+  def test_order_object_is_instance_of_order
+    @order = Order.new
+    assert_instance_of(Order, @order)
+  end
 end
